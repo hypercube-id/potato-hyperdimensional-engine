0184c795716e3f5979b35e60385d7ea2aafdef7d
Hypercube <hypercube.id@gmail.com>
2026-07-14 15:43:11 +0700

    Update hdc_server-alpha-v1.py

--- hdc_server-alpha-v1.py
+++ hdc_server-alpha-v1.py
@@ -1,7 +1,7 @@
 # =====================================================================
 #  Project PHDE: Potato HyperDimensional Engine
 #  Copyright (C) 2026 The Hub Studio
-#  GPLv3
+#  AGPLv3
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
